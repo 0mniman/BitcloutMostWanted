63e85d6f2dc83fa3dd8444f73f3ee1c040a00ec3
Harman <68689931+Harman36@users.noreply.github.com>
2021-04-14 19:55:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function run()
+{
+ 
+    var e = document.getElementById("option");
+    var thevalue = e.value;
+    if(thevalue == "search")
+    {
+        console.log("search");
+    }
+
+    else
+    {
+        console.log("report");
+    }
+}
